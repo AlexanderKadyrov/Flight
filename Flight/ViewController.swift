bc30a8f41bc193d8e1086b1470d83beaa06abe5c
Alexander <alexander@MacBook-Pro-A.local>
2018-10-20 12:31:19 +0300

    initial commit

---
+++ Flight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flight
+//
+//  Created by Alexander on 20/10/2018.
+//  Copyright © 2018 Alexander Kadyrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
