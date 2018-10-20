4f7d53ad1443982cea49dbc76785c44aa4bb64d3
Alexander Kadyrov <alexander-kad@yandex.ru>
2018-10-21 00:52:42 +0300

    add UIColor+Extension.swift

---
+++ Flight/Sources/Classes/Extensions/UIColor/UIColor+Extension.swift
@@ -0,0 +1,36 @@
+//
+//  UIColor+Extension.swift
+//  Flight
+//
+//  Created by Alexander on 21/10/2018.
+//  Copyright © 2018 Alexander Kadyrov. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let colorJapaneseIndigo = color(hex: "264653")
+    static let colorJungleGreen = color(hex: "2A9D8F")
+    static let colorHansaYellow = color(hex: "E9C46A")
+    static let colorSandyBrown = color(hex: "F4A261")
+    static let colorLightRedOchre = color(hex: "E76F51")
+    
+    private static func color(hex: String) -> UIColor {
+        var cString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        if cString.hasPrefix("#") {
+            cString.remove(at: cString.startIndex)
+        }
+        if (cString.count) != 6 {
+            return UIColor.gray
+        }
+        var rgbValue: UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
