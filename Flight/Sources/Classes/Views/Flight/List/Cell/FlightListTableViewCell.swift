8684db561041bf1986303409c4416e75b1081092
Alexander Kadyrov <alexander-kad@yandex.ru>
2018-10-21 01:10:42 +0300

    work with FlightListTableViewCell

--- Flight/Sources/Classes/Views/Flight/List/Cell/FlightListTableViewCell.swift
+++ Flight/Sources/Classes/Views/Flight/List/Cell/FlightListTableViewCell.swift
@@ -10,17 +10,20 @@ import UIKit
 
 class FlightListTableViewCell: UITableViewCell {
     
+    @IBOutlet private weak var viewContainer: UIView!
     @IBOutlet private weak var labelTitle: UILabel!
     @IBOutlet private weak var labelSubtitle: UILabel!
     
     //настраиваем стили и/или логику
     override func awakeFromNib() {
         super.awakeFromNib()
+        viewContainer.layer.cornerRadius = 6.0
     }
     
     //объявляем свойство и слушаем setter
     public var item: FlightProtocol! {
         didSet {
+            viewContainer.backgroundColor = item.color
             labelTitle.text = item.title
             labelSubtitle.text = item.subtitle
         }
