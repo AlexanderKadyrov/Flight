96e47f9df0705148b3491aef794e4869cc564eb3
Alexander Kadyrov <alexander-kad@yandex.ru>
2018-10-21 14:15:41 +0300

    work with MoveDetailViewController

--- Flight/Sources/Classes/ViewControllers/Move/Detail/MoveDetailViewController.swift
+++ Flight/Sources/Classes/ViewControllers/Move/Detail/MoveDetailViewController.swift
@@ -6,17 +6,33 @@
 //  Copyright © 2018 Alexander Kadyrov. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class MoveDetailViewController: FlightDetailViewController {
+    
+    
+    @IBOutlet private weak var viewContainerAirplaneTakeoff: UIView!
+    @IBOutlet private weak var viewContainerAirplaneLanding: UIView!
     
     //настраиваем стили и/или логику
     override func viewDidLoad() {
         super.viewDidLoad()
         makeToolbar()
+        makeItems()
     }
     
     private func makeToolbar() {
         setBarTitle("Move")
+    }
+    
+    private func makeItems() {
+        viewContainerAirplaneTakeoff.layer.borderColor = UIColor.white.cgColor
+        viewContainerAirplaneTakeoff.backgroundColor = .colorOnyx
+        viewContainerAirplaneTakeoff.layer.cornerRadius = 38
+        viewContainerAirplaneTakeoff.layer.borderWidth = 3
+        viewContainerAirplaneLanding.layer.borderColor = UIColor.white.cgColor
+        viewContainerAirplaneLanding.backgroundColor = .colorOnyx
+        viewContainerAirplaneLanding.layer.cornerRadius = 38
+        viewContainerAirplaneLanding.layer.borderWidth = 3
     }
 }
