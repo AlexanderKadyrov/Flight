e6e04df20c37a47e94d274451150a2deb5b3bf95
Alexander Kadyrov <alexander-kad@yandex.ru>
2018-10-21 13:14:55 +0300

    work with NoticeDetailViewController

--- Flight/Sources/Classes/ViewControllers/Notice/Detail/NoticeDetailViewController.swift
+++ Flight/Sources/Classes/ViewControllers/Notice/Detail/NoticeDetailViewController.swift
@@ -6,17 +6,39 @@
 //  Copyright © 2018 Alexander Kadyrov. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class NoticeDetailViewController: FlightDetailViewController {
+    
+    @IBOutlet private weak var labelTitle: UILabel!
+    @IBOutlet private weak var labelSubtitle: UILabel!
+    
+    override var item: FlightProtocol! {
+        didSet {
+            guard let thisItem = item as? Notice else { return }
+            if let thisGate = thisItem.gate, !thisGate.isEmpty {
+                labelTitle.text = thisGate
+            }
+            if let thisFlightDate = thisItem.flightDate {
+                labelSubtitle.text = thisFlightDate.toString(.formatterDate)
+            }
+        }
+    }
     
     //настраиваем стили и/или логику
     override func viewDidLoad() {
         super.viewDidLoad()
         makeToolbar()
+        makeItems()
     }
     
     private func makeToolbar() {
         setBarTitle("Notice")
+    }
+    
+    private func makeItems() {
+        labelTitle.layer.borderColor = UIColor.white.cgColor
+        labelTitle.layer.cornerRadius = 70
+        labelTitle.layer.borderWidth = 5
     }
 }
