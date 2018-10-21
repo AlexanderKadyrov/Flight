34be333aee0b49340143d4e57de0d8cfa145e104
Alexander Kadyrov <alexander-kad@yandex.ru>
2018-10-21 11:35:56 +0300

    work with AppDelegate.swift

--- Flight/Sources/Classes/Controllers/Base/AppDelegate.swift
+++ Flight/Sources/Classes/Controllers/Base/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let nv = NavigationViewController(vc, color: .colorOnyx)
         window?.rootViewController = nv
         window?.makeKeyAndVisible()
+        window?.tintColor = .white
         return true
     }
 }
