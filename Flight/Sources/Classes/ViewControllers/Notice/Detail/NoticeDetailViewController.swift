7f057fe4f27bbdc753a30aadbd123ef927a87070
Alexander Kadyrov <alexander-kad@yandex.ru>
2018-10-21 13:32:43 +0300

    work with NoticeDetailViewController.swift

--- Flight/Sources/Classes/ViewControllers/Notice/Detail/NoticeDetailViewController.swift
+++ Flight/Sources/Classes/ViewControllers/Notice/Detail/NoticeDetailViewController.swift
@@ -13,23 +13,12 @@ class NoticeDetailViewController: FlightDetailViewController {
     @IBOutlet private weak var labelTitle: UILabel!
     @IBOutlet private weak var labelSubtitle: UILabel!
     
-    override var item: FlightProtocol! {
-        didSet {
-            guard let thisItem = item as? Notice else { return }
-            if let thisGate = thisItem.gate, !thisGate.isEmpty {
-                labelTitle.text = thisGate
-            }
-            if let thisFlightDate = thisItem.flightDate {
-                labelSubtitle.text = thisFlightDate.toString(.formatterDate)
-            }
-        }
-    }
-    
     //настраиваем стили и/или логику
     override func viewDidLoad() {
         super.viewDidLoad()
         makeToolbar()
         makeItems()
+        setData()
     }
     
     private func makeToolbar() {
@@ -37,8 +26,18 @@ class NoticeDetailViewController: FlightDetailViewController {
     }
     
     private func makeItems() {
-        labelTitle.layer.borderColor = UIColor.white.cgColor
+        labelTitle.layer.borderColor = UIColor.colorBrilliantAzure.cgColor
         labelTitle.layer.cornerRadius = 70
         labelTitle.layer.borderWidth = 5
+    }
+    
+    private func setData() {
+        guard let thisItem = item as? Notice else { return }
+        if let thisGate = thisItem.gate, !thisGate.isEmpty {
+            labelTitle.text = thisGate
+        }
+        if let thisFlightDate = thisItem.flightDate {
+            labelSubtitle.text = thisFlightDate.toString(.formatterDate)
+        }
     }
 }
