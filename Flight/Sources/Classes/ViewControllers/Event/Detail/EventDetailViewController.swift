8be280be2323a1a0ee6ee6a77f8b14b6dcc0e28e
Alexander Kadyrov <alexander-kad@yandex.ru>
2018-10-21 13:06:10 +0300

    work with EventDetailViewController

--- Flight/Sources/Classes/ViewControllers/Event/Detail/EventDetailViewController.swift
+++ Flight/Sources/Classes/ViewControllers/Event/Detail/EventDetailViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class EventDetailViewController: FlightDetailViewController {
     
+    @IBOutlet private weak var labelTitle: UILabel!
+    @IBOutlet private weak var labelSubtitle: UILabel!
+    
+    override var item: FlightProtocol! {
+        didSet {
+            guard let thisItem = item as? Event else { return }
+        }
+    }
+    
     //настраиваем стили и/или логику
     override func viewDidLoad() {
         super.viewDidLoad()
