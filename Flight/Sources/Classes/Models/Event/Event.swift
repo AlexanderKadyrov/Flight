8bf465b59ef22d27227e9cab00c5aa006b6040f7
Alexander Kadyrov <a.kadyrov@alt.software>
2024-01-04 02:59:41 +0500

    fixes

--- Flight/Sources/Classes/Models/Event/Event.swift
+++ Flight/Sources/Classes/Models/Event/Event.swift
@@ -7,60 +7,27 @@
 //
 
 import Foundation
-import UIKit
 
-fileprivate let dateFormatEvent = "dd-MM-yyyy HH:mm:ss"
-
-enum Airline: String, Codable {
-    case uralairlines
-    case transaero
-    case aeroflot
-    case redwings
-    case nordstar
-    case pegasfly
-    case openair
-    case kolavia
-    case pobeda
-    case utair
-    case s7
-}
-
-class Event: Codable {
+struct Event: Codable {
     
-    var startTime: Date?
-    var endTime: Date?
-    var airline: Airline!
-    var name: String!
-    
-    enum CodingKeys: String, CodingKey {
-        case startTime
-        case endTime
-        case airline
-        case name
+    enum Airline: String, Codable {
+        case uralairlines
+        case transaero
+        case aeroflot
+        case redwings
+        case nordstar
+        case pegasfly
+        case openair
+        case kolavia
+        case pobeda
+        case utair
+        case s7
     }
     
-    required init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        
-        let stringStartTime = try values.decode(String.self, forKey: .startTime)
-        startTime = Date.with(value: stringStartTime, format: dateFormatEvent)
-        
-        let stringEndTime = try values.decode(String.self, forKey: .endTime)
-        endTime = Date.with(value: stringEndTime, format: dateFormatEvent)
-        
-        let stringAirline = try values.decode(String.self, forKey: .airline)
-        airline = Airline(rawValue: stringAirline)
-        
-        name = try values.decode(String.self, forKey: .name)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(startTime, forKey: .startTime)
-        try container.encode(endTime, forKey: .endTime)
-        try container.encode(airline, forKey: .airline)
-        try container.encode(name, forKey: .name)
-    }
+    var startTime: Date
+    var endTime: Date
+    var airline: Airline
+    var name: String
 }
 
 extension Event: FlightProtocol {
@@ -70,16 +37,11 @@ extension Event: FlightProtocol {
     }
     
     var subtitle: String {
-        var values = [String]()
-        if !name.isEmpty {
-            values.append(name)
-        }
-        if let thisStartTime = startTime {
-            values.append(thisStartTime.toString(.formatterTime))
-        }
-        if let thisEndTime = endTime {
-            values.append(thisEndTime.toString(.formatterTime))
-        }
+        var values: [String] = [
+            name,
+            startTime.toString(.formatterTime),
+            endTime.toString(.formatterTime)
+        ]
         return values.joined(separator: "\n")
     }
 }
