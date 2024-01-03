64db95a2023de31eedacc1b418b36b0a5bd75dea
Alexander Kadyrov <a.kadyrov@alt.software>
2024-01-04 03:25:31 +0500

    fixes

--- Flight/Sources/Classes/ViewControllers/Flight/Detail/Event/Detail/EventDetailViewController.swift
+++ Flight/Sources/Classes/ViewControllers/Flight/Detail/Event/Detail/EventDetailViewController.swift
@@ -15,22 +15,25 @@ class EventDetailViewController: BaseViewController, FlightDetailViewControllerP
     @IBOutlet private weak var labelStartTime: UILabel!
     @IBOutlet private weak var labelEndTime: UILabel!
     
-    var item: FlightProtocol?
+    var item: FlightProtocol? {
+        didSet {
+            guard let item = item as? Event else { return }
+            setNavigationBar(text: item.name)
+            
+            let nameLogo = "icon_logo_" + item.airline.rawValue
+            imageViewLogo.image = UIImage(named: nameLogo)
+            
+            labelStartTime.text = "event_start_time_title".localized() + ": " + item.startTime.toString(.formatterTime)
+            labelEndTime.text = "event_end_time_title".localized() + ": " + item.endTime.toString(.formatterTime)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        makeToolbar()
-        makeItems()
-        setData()
+        configureViews()
     }
     
-    private func makeToolbar() {
-        guard let thisItem = item as? Event else { return }
-        setBarTitle(thisItem.name)
-    }
-    
-    private func makeItems() {
-        
+    private func configureViews() {
         viewContent.layer.borderColor = UIColor.white.cgColor
         viewContent.layer.cornerRadius = 140
         viewContent.layer.borderWidth = 5
@@ -39,13 +42,5 @@ class EventDetailViewController: BaseViewController, FlightDetailViewControllerP
         imageViewLogo.layer.masksToBounds = true
         imageViewLogo.layer.cornerRadius = 120
         imageViewLogo.backgroundColor = .colorOnyx
-    }
-    
-    private func setData() {
-        guard let item = item as? Event else { return }
-        let nameLogo = "icon_logo_" + item.airline.rawValue
-        imageViewLogo.image = UIImage(named: nameLogo)
-        labelStartTime.text = "event_start_time_title".localized() + ": " + item.startTime.toString(.formatterTime)
-        labelEndTime.text = "event_end_time_title".localized() + ": " + item.endTime.toString(.formatterTime)
     }
 }
