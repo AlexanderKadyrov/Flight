b55e782da030c12bfa2f9d2b01e63d54774383fa
Alexander Kadyrov <a.kadyrov@alt.software>
2024-01-04 01:30:45 +0500

    fixes

--- Flight/Sources/Classes/ViewControllers/Flight/List/Cell/FlightListTableViewCell.swift
+++ Flight/Sources/Classes/ViewControllers/Flight/List/Cell/FlightListTableViewCell.swift
@@ -10,23 +10,42 @@ import UIKit
 
 class FlightListTableViewCell: UITableViewCell {
     
-    @IBOutlet private weak var viewContainer: UIView!
+    private let containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .colorBrilliantAzure
+        view.layer.cornerRadius = 6.0
+        return view
+    }()
+    
     @IBOutlet private weak var labelTitle: UILabel!
     @IBOutlet private weak var labelSubtitle: UILabel!
     
-    //настраиваем стили и/или логику
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        viewContainer.layer.cornerRadius = 6.0
-    }
-    
-    //объявляем свойство и слушаем setter
-    public var item: FlightProtocol? {
+    var item: FlightProtocol? {
         didSet {
             guard let thisItem = item else { return }
-            viewContainer.backgroundColor = .colorBrilliantAzure
             labelTitle.text = thisItem.title
             labelSubtitle.text = thisItem.subtitle
         }
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureViews()
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureViews()
+    }
+    
+    private func configureViews() {
+        contentView.addSubview(containerView)
+        NSLayoutConstraint.activate([
+            containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12),
+            containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -12),
+            containerView.topAnchor.constraint(equalTo: contentView.topAnchor)
+        ])
     }
 }
