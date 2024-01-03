c83f32d4ff3301fb8773a0e6a99842ee212d5c80
Alexander Kadyrov <a.kadyrov@alt.software>
2024-01-04 04:19:37 +0500

    fixes

--- Flight/Sources/Classes/ViewControllers/Flight/Detail/Notice/Detail/NoticeDetailViewController.swift
+++ Flight/Sources/Classes/ViewControllers/Flight/Detail/Notice/Detail/NoticeDetailViewController.swift
@@ -10,31 +10,59 @@ import UIKit
 
 class NoticeDetailViewController: BaseViewController, FlightDetailViewControllerProtocol {
     
-    @IBOutlet private weak var labelTitle: UILabel!
-    @IBOutlet private weak var labelSubtitle: UILabel!
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 51, weight: .medium)
+        label.layer.borderColor = UIColor.colorBrilliantAzure.cgColor
+        label.textAlignment = .center
+        label.layer.cornerRadius = 70
+        label.layer.borderWidth = 5
+        label.textColor = .white
+        return label
+    }()
     
-    var item: FlightProtocol?
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 21)
+        label.textAlignment = .center
+        label.textColor = .white
+        return label
+    }()
+    
+    var item: FlightProtocol? {
+        didSet {
+            guard let item = item as? Notice else { return }
+            titleLabel.text = item.gate
+            subtitleLabel.text = item.flightDate.toString(.formatterDate)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        makeToolbar()
-        makeItems()
-        setData()
+        configureToolbar()
+        configureViews()
     }
     
-    private func makeToolbar() {
+    private func configureToolbar() {
         setNavigationBar(text: "Notice")
     }
     
-    private func makeItems() {
-        labelTitle.layer.borderColor = UIColor.colorBrilliantAzure.cgColor
-        labelTitle.layer.cornerRadius = 70
-        labelTitle.layer.borderWidth = 5
-    }
-    
-    private func setData() {
-        guard let item = item as? Notice else { return }
-        labelTitle.text = item.gate
-        labelSubtitle.text = item.flightDate.toString(.formatterDate)
+    private func configureViews() {
+        view.addSubview(titleLabel)
+        view.addSubview(subtitleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -64),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.heightAnchor.constraint(equalToConstant: 140),
+            titleLabel.widthAnchor.constraint(equalToConstant: 140),
+            
+            subtitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            subtitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            subtitleLabel.heightAnchor.constraint(equalToConstant: 45)
+        ])
     }
 }
