37d0ac9f0d706acb9cafa555771dd77f116ab6e0
Alexander Kadyrov <a.kadyrov@alt.software>
2024-01-04 04:25:42 +0500

    fixes

--- Flight/Sources/Classes/Controllers/Navigation/NavigationController.swift
+++ Flight/Sources/Classes/Controllers/Navigation/NavigationController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class NavigationViewController: UINavigationController {
+class NavigationController: UINavigationController {
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
