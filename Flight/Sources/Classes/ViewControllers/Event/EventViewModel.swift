ad7116ca4322f15bf358b466abe2a8a44cadb770
Alexander Kadyrov <a.kadyrov@alt.software>
2024-01-05 04:21:57 +0500

    fixes

--- Flight/Sources/Classes/ViewControllers/Event/EventViewModel.swift
+++ Flight/Sources/Classes/ViewControllers/Event/EventViewModel.swift
@@ -9,18 +9,31 @@
 import Foundation
 import UIKit
 
-final class EventViewModel {
+final class EventViewModel: ViewModelProtocol {
     
-    let logoImage: UIImage?
+    private(set) var logoImage: UIImage?
     
-    let startTime: String
-    let endTime: String
-    let name: String
+    private(set) var startTime: String?
+    private(set) var endTime: String?
+    private(set) var name: String?
     
-    init(model: Event) {
-        logoImage = UIImage(named: "icon_logo_\(model.airline.rawValue)")
-        startTime = "event_start_time_title".localized() + ": " + model.startTime.toString(.formatterTime)
-        endTime = "event_end_time_title".localized() + ": " + model.endTime.toString(.formatterTime)
-        name = model.name
+    var model: Event? {
+        didSet {
+            logoImage = configureLogoImage(airline: model?.airline)
+            startTime = configureTime(components: ["event_start_time_title".localized(), model?.startTime.toString(.formatterTime)])
+            endTime = configureTime(components: ["event_end_time_title".localized(), model?.endTime.toString(.formatterTime)])
+            name = model?.name
+        }
+    }
+    
+    private func configureLogoImage(airline: Event.Airline?) -> UIImage? {
+        guard let airline = airline else { return nil }
+        return UIImage(named: "icon_logo_\(airline.rawValue)")
+    }
+    
+    private func configureTime(components: [String?]) -> String? {
+        return components
+            .compactMap { $0 }
+            .joined(separator: ": ")
     }
 }
