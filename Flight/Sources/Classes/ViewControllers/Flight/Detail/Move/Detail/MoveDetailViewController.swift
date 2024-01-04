3b2114f6077b3afce379ee33812832c5c80edf92
Alexander Kadyrov <a.kadyrov@alt.software>
2024-01-04 05:10:41 +0500

    fixes

--- Flight/Sources/Classes/ViewControllers/Flight/Detail/Move/Detail/MoveDetailViewController.swift
+++ Flight/Sources/Classes/ViewControllers/Flight/Detail/Move/Detail/MoveDetailViewController.swift
@@ -10,41 +10,76 @@ import UIKit
 
 class MoveDetailViewController: BaseViewController, FlightDetailViewControllerProtocol {
     
+    /*
     @IBOutlet private weak var viewContainerAirplaneTakeoff: UIView!
     @IBOutlet private weak var viewContainerAirplaneLanding: UIView!
     @IBOutlet private weak var labelFromPlace: UILabel!
     @IBOutlet private weak var labelToPlace: UILabel!
-    @IBOutlet private weak var labelTime: UILabel!
+    @IBOutlet private weak var labelTime: UILabel!*/
     
-    var item: FlightProtocol?
+    private let moveView: MoveView = {
+        let view = MoveView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
-    //настраиваем стили и/или логику
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        makeToolbar()
-        makeItems()
-        setData()
+    private let fromPlaceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 17, weight: .medium)
+        label.textAlignment = .left
+        label.textColor = .white
+        return label
+    }()
+    
+    private let toPlaceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 17, weight: .medium)
+        label.textAlignment = .right
+        label.textColor = .white
+        return label
+    }()
+    
+    var item: FlightProtocol? {
+        didSet {
+            guard let item = item as? Move else { return }
+            fromPlaceLabel.text = item.fromPlace
+            toPlaceLabel.text = item.toPlace
+            //labelTime.text = item.estimateTime.toString
+        }
     }
     
-    private func makeToolbar() {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureToolbar()
+        configureViews()
+    }
+    
+    private func configureToolbar() {
         setNavigationBar(text: "Move")
     }
     
-    private func makeItems() {
-        viewContainerAirplaneTakeoff.layer.borderColor = UIColor.colorBrilliantAzure.cgColor
-        viewContainerAirplaneTakeoff.backgroundColor = .colorOnyx
-        viewContainerAirplaneTakeoff.layer.cornerRadius = 38
-        viewContainerAirplaneTakeoff.layer.borderWidth = 3
-        viewContainerAirplaneLanding.layer.borderColor = UIColor.colorBrilliantAzure.cgColor
-        viewContainerAirplaneLanding.backgroundColor = .colorOnyx
-        viewContainerAirplaneLanding.layer.cornerRadius = 38
-        viewContainerAirplaneLanding.layer.borderWidth = 3
-    }
-    
-    private func setData() {
-        guard let item = item as? Move else { return }
-        labelFromPlace.text = item.fromPlace
-        labelToPlace.text = item.toPlace
-        labelTime.text = item.estimateTime.toString
+    private func configureViews() {
+        view.addSubview(moveView)
+        view.addSubview(fromPlaceLabel)
+        view.addSubview(toPlaceLabel)
+        
+        NSLayoutConstraint.activate([
+            moveView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            moveView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -40),
+            moveView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            moveView.heightAnchor.constraint(equalToConstant: 76),
+            
+            fromPlaceLabel.bottomAnchor.constraint(equalTo: moveView.topAnchor, constant: -20),
+            fromPlaceLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            fromPlaceLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            fromPlaceLabel.heightAnchor.constraint(equalToConstant: 45),
+            
+            toPlaceLabel.topAnchor.constraint(equalTo: moveView.bottomAnchor, constant: 20),
+            toPlaceLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            toPlaceLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            toPlaceLabel.heightAnchor.constraint(equalToConstant: 45)
+        ])
     }
 }
