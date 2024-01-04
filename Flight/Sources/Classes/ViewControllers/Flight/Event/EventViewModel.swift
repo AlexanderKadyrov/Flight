74d3132696fdfb0da25f615569ebeb9393955813
Alexander Kadyrov <a.kadyrov@alt.software>
2024-01-05 03:39:38 +0500

    fixes

--- Flight/Sources/Classes/ViewControllers/Flight/Event/EventViewModel.swift
+++ Flight/Sources/Classes/ViewControllers/Flight/Event/EventViewModel.swift
@@ -7,14 +7,18 @@
 //
 
 import Foundation
+import UIKit
 
 final class EventViewModel {
+    
+    let logoImage: UIImage?
     
     let startTime: String
     let endTime: String
     let name: String
     
     init(model: Event) {
+        logoImage = UIImage(named: "icon_logo_\(model.airline.rawValue)")
         startTime = "event_start_time_title".localized() + ": " + model.startTime.toString(.formatterTime)
         endTime = "event_end_time_title".localized() + ": " + model.endTime.toString(.formatterTime)
         name = model.name
