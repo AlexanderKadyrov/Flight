958d4f10278be3f87abe1d8c9731317624f55969
Alexander Kadyrov <a.kadyrov@alt.software>
2024-01-05 03:37:10 +0500

    work with EventViewModel

---
+++ Flight/Sources/Classes/ViewControllers/Flight/Event/EventViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  EventViewModel.swift
+//  Flight
+//
+//  Created by Alexander on 05.01.2024.
+//  Copyright © 2024 Alexander Kadyrov. All rights reserved.
+//
+
+import Foundation
+
+final class EventViewModel {
+    
+    let startTime: String
+    let endTime: String
+    
+    init(model: Event) {
+        startTime = "event_start_time_title".localized() + ": " + model.startTime.toString(.formatterTime)
+        endTime = "event_end_time_title".localized() + ": " + model.endTime.toString(.formatterTime)
+    }
+}
