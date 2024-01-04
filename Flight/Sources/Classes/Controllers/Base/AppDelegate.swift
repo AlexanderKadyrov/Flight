2406c6ec2b79aa431ce1e3ee55af4a6d492bf71b
Alexander Kadyrov <a.kadyrov@alt.software>
2024-01-04 21:05:58 +0500

    fixes

--- Flight/Sources/Classes/Controllers/Base/AppDelegate.swift
+++ Flight/Sources/Classes/Controllers/Base/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         let viewController = FlightListViewController()
+        let viewModel = FlightListViewModel()
+        viewController.viewModel = viewModel
+        
         let navigationController = NavigationController(rootViewController: viewController, color: .colorOnyx)
         
         window = UIWindow(frame: UIScreen.main.bounds)
