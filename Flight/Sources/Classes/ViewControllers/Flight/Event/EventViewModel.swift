4da28a3fa55a2e6771216a69ea2ce87dcaa12a66
Alexander Kadyrov <a.kadyrov@alt.software>
2024-01-05 03:37:54 +0500

    fixes

--- Flight/Sources/Classes/ViewControllers/Flight/Event/EventViewModel.swift
+++ Flight/Sources/Classes/ViewControllers/Flight/Event/EventViewModel.swift
@@ -12,9 +12,11 @@ final class EventViewModel {
     
     let startTime: String
     let endTime: String
+    let name: String
     
     init(model: Event) {
         startTime = "event_start_time_title".localized() + ": " + model.startTime.toString(.formatterTime)
         endTime = "event_end_time_title".localized() + ": " + model.endTime.toString(.formatterTime)
+        name = model.name
     }
 }
